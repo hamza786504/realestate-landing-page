abf2a56e1087545ce517d2eaad524096d70c1000
hamza <hamza786504@gmail.com>
2023-11-24 23:47:02 +0500

    bootrap toast added along with javascript for form

--- js/script.js
+++ js/script.js
@@ -29,12 +29,6 @@ function setSpeed() {
 
 setTimeout(setSpeed, 1000);
 
-
-
-
-
-
-
 function showContent(contentId) {
   const buttons = document.querySelectorAll(".toggle-button");
   buttons.forEach((button) => button.classList.remove("active"));
@@ -51,154 +45,168 @@ function showContent(contentId) {
   activeContent.classList.add("active");
 }
 
-
-
-
-
-
-
 function validateForm() {
-    var name = document.getElementById('user_name').value;
-    var email = document.getElementById('email').value;
-    var request = document.getElementById('request').value;
+  var name = document.getElementById("user_name").value;
+  var email = document.getElementById("email").value;
+  var request = document.getElementById("request").value;
 
-    // Reset error messages
-    document.getElementById('nameError').innerHTML = '';
-    document.getElementById('emailError').innerHTML = '';
-    document.getElementById('requestError').innerHTML = '';
+  // Reset error messages
+  document.getElementById("nameError").innerHTML = "";
+  document.getElementById("emailError").innerHTML = "";
+  document.getElementById("requestError").innerHTML = "";
 
-    // Validate name
-    if (name.trim() === '') {
-      document.getElementById('nameError').innerHTML = 'Name is required';
-      return false;
-    }
-
-    // Validate email
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      document.getElementById('emailError').innerHTML = 'Invalid email address';
-      return false;
-    }
-
-    // Validate request
-    if (request.trim() === '') {
-      document.getElementById('requestError').innerHTML = 'Request is required';
-      return false;
-    }
-
-    // Form is valid, send data
-    sendData({ name, email, request });
-
-    // Prevent form submission (optional)
+  // Validate name
+  if (name.trim() === "") {
+    document.getElementById("nameError").innerHTML = "Name is required";
     return false;
   }
 
-  function sendData(data) {
-    // Replace 'https://example.com' with the actual URL
-    var url = 'https://aigold.co.uk';
-    console.log(JSON.stringify(data));
+  // Validate email
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    document.getElementById("emailError").innerHTML = "Invalid email address";
+    return false;
+  }
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    .then(response => {
+  // Validate request
+  if (request.trim() === "") {
+    document.getElementById("requestError").innerHTML = "Request is required";
+    return false;
+  }
+
+  // Form is valid, send data
+  sendData({ name, email, request });
+
+  // Prevent form submission (optional)
+  return false;
+}
+
+function sendData(data) {
+  // Replace 'https://example.com' with the actual URL
+  var url = "https://aigold.co.uk";
+  console.log(JSON.stringify(data));
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        showToast("Error!", "Network response was not ok", "bg-danger");
+        throw new Error("Network response was not ok");
       }
-      // You can handle success here, e.g., show a success message
-      console.log('Data sent successfully');
+      // Show success toast on success
+      showToast("Success!", "Thanks for reaching us!!", "bg-success");
     })
-    .catch(error => {
-      // Handle error here, e.g., show an error message
-      console.error('There was a problem with the fetch operation:', error);
+    .catch((error) => {
+      // Show error toast on failure
+      showToast(
+        "Error!",
+        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
+        "bg-danger"
+      );
+      console.error(
+        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
+        error
+      );
     });
-  }
+}
 
+function validateBannerForm() {
+  var bannerEmail = document.getElementById("banner_email").value;
 
+  // Reset error message
+  document.getElementById("bannerEmailError").innerHTML = "";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  function validateBannerForm() {
-    var bannerEmail = document.getElementById('banner_email').value;
-
-    // Reset error message
-    document.getElementById('bannerEmailError').innerHTML = '';
-
-    // Validate email address
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(bannerEmail)) {
-      document.getElementById('bannerEmailError').innerHTML = 'Invalid email address';
-      return false;
-    }
-
-    // Form is valid, send data
-    sendBannerFormData({ email: bannerEmail });
-
-    // Prevent form submission (optional)
-    return false;
-  }
-  function validateNewsletterForm() {
-    var newsletterEmail = document.getElementById('newsletter_email').value;
-
-    // Reset error message
-    document.getElementById('newsletterEmailError').innerHTML = '';
-
-    // Validate email address
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(newsletterEmail)) {
-      document.getElementById('newsletterEmailError').innerHTML = 'Invalid email address';
-      return false;
-    }
-
-    // Form is valid, send data
-    sendBannerFormData({ email: newsletterEmail });
-
-    // Prevent form submission (optional)
+  // Validate email address
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(bannerEmail)) {
+    document.getElementById("bannerEmailError").innerHTML =
+      "Invalid email address";
     return false;
   }
 
-  function sendBannerFormData(data) {
-    // Replace 'https://example.com' with the actual URL
-    var url = 'https://aigold.co.uk';
+  // Form is valid, send data
+  sendBannerFormData({ email: bannerEmail });
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    .then(response => {
+  // Prevent form submission (optional)
+  return false;
+}
+function validateNewsletterForm() {
+  var newsletterEmail = document.getElementById("newsletter_email").value;
+
+  // Reset error message
+  document.getElementById("newsletterEmailError").innerHTML = "";
+
+  // Validate email address
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(newsletterEmail)) {
+    document.getElementById("newsletterEmailError").innerHTML =
+      "Invalid email address";
+    return false;
+  }
+
+  // Form is valid, send data
+  sendBannerFormData({ email: newsletterEmail });
+
+  // Prevent form submission (optional)
+  return false;
+}
+
+function sendBannerFormData(data) {
+  var url = "https://aigold.co.uk";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        showToast("Error!", "Network response was not ok", "bg-danger");
+        throw new Error("Network response was not ok");
       }
-      // You can handle success here, e.g., show a success message
-      console.log('Banner form data sent successfully');
+      // Show success toast on success
+      showToast("Success!", "Thanks for reaching us!!", "bg-success");
     })
-    .catch(error => {
-      // Handle error here, e.g., show an error message
-      console.error('There was a problem with the fetch operation:', error);
+    .catch((error) => {
+      // Show error toast on failure
+      showToast(
+        "Error!",
+        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
+        "bg-danger"
+      );
+      console.error(
+        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
+        error
+      );
     });
-  }
+}
+
+function showToast(title, message, bgColorClass) {
+  // Use Bootstrap toast to show success or error message
+  var toastContainer = document.getElementById("toastContainer");
+  var toast = new bootstrap.Toast(toastContainer);
+
+  // Update toast content
+  toastContainer.querySelector(".toast-title").innerText = title;
+  toastContainer.querySelector(".toast-body").innerText = message;
+
+  // Update toast background color class
+  toastContainer.classList.remove("bg-success", "bg-danger");
+  toastContainer.classList.add(bgColorClass);
+
+
+    var forms = document.forms;
+    for (var i = 0; i < forms.length; i++) {
+        forms[i].reset();
+    }
+
+  // Show the toast
+  toast.show();
+}
