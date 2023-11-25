f4f911c179cfb1bc2616a3487a6acfb5c40bd559
hamza <hamza786504@gmail.com>
2023-11-25 05:26:37 +0500

    success and faliure message along with their functionality was added instead of toast

--- js/script.js
+++ js/script.js
@@ -45,7 +45,66 @@ function showContent(contentId) {
   activeContent.classList.add("active");
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeError(){
+  document.querySelectorAll(".form-message").forEach(result => {
+    result.innerHTML = "";
+  });
+}
+
+
+
+
+
+
+
+
+// Validation and form submission for Form 1
+function validateNewsletterForm() {
+  removeError();
+  var newsletterEmail = document.getElementById("newsletter_email").value;
+
+  // Reset error message
+  document.getElementById("newsletterEmailError").innerHTML = "";
+
+  // Validate email address
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(newsletterEmail)) {
+      showFormMessage("newsletterEmailError", "Error", "Invalid email address", "text-danger");     
+      return false;
+  }
+
+  // Form is valid, send data
+  sendFormData("formContainer1", { email: newsletterEmail });
+
+  // Prevent form submission (optional)
+  return false;
+}
+
+// Validation and form submission for Form 2
 function validateForm() {
+  removeError();
   var name = document.getElementById("user_name").value;
   var email = document.getElementById("email").value;
   var request = document.getElementById("request").value;
@@ -57,156 +116,97 @@ function validateForm() {
 
   // Validate name
   if (name.trim() === "") {
-    document.getElementById("nameError").innerHTML = "Name is required";
-    return false;
+      showFormMessage("nameError", "Error", "Name is required", "text-danger");
+      return false;
   }
 
   // Validate email
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-    document.getElementById("emailError").innerHTML = "Invalid email address";
-    return false;
+    showFormMessage("emailError", "Error", "Invalid email address", "text-danger");
+      return false;
   }
 
   // Validate request
   if (request.trim() === "") {
-    document.getElementById("requestError").innerHTML = "Request is required";
-    return false;
+      showFormMessage("requestError", "Error", "Request is required", "text-danger");
+      return false;
   }
 
   // Form is valid, send data
-  sendData({ name, email, request });
+  sendFormData("formContainer2", { name, email, request });
 
   // Prevent form submission (optional)
   return false;
 }
 
-function sendData(data) {
-  // Replace 'https://example.com' with the actual URL
-  var url = "https://aigold.co.uk";
-  console.log(JSON.stringify(data));
-
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        showToast("Error!", "Network response was not ok", "bg-danger");
-        throw new Error("Network response was not ok");
-      }
-      // Show success toast on success
-      showToast("Success!", "Thanks for reaching us!!", "bg-success");
-    })
-    .catch((error) => {
-      // Show error toast on failure
-      showToast(
-        "Error!",
-        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
-        "bg-danger"
-      );
-      console.error(
-        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
-        error
-      );
-    });
-}
-
+// Validation and form submission for Form 3
 function validateBannerForm() {
+  removeError();
   var bannerEmail = document.getElementById("banner_email").value;
-
-  // Reset error message
-  document.getElementById("bannerEmailError").innerHTML = "";
 
   // Validate email address
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(bannerEmail)) {
-    document.getElementById("bannerEmailError").innerHTML =
-      "Invalid email address";
-    return false;
+      showFormMessage("formContainer3", "Error", "Invalid email address", "text-danger");
+
+      return false;
   }
 
   // Form is valid, send data
-  sendBannerFormData({ email: bannerEmail });
-
-  // Prevent form submission (optional)
-  return false;
-}
-function validateNewsletterForm() {
-  var newsletterEmail = document.getElementById("newsletter_email").value;
-
-  // Reset error message
-  document.getElementById("newsletterEmailError").innerHTML = "";
-
-  // Validate email address
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(newsletterEmail)) {
-    document.getElementById("newsletterEmailError").innerHTML =
-      "Invalid email address";
-    return false;
-  }
-
-  // Form is valid, send data
-  sendBannerFormData({ email: newsletterEmail });
+  sendFormData("formContainer3", { email: bannerEmail });
 
   // Prevent form submission (optional)
   return false;
 }
 
-function sendBannerFormData(data) {
+// Function to send form data using fetch
+function sendFormData(containerId, data) {
   var url = "https://aigold.co.uk";
 
   fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
   })
-    .then((response) => {
-      if (!response.ok) {
-        showToast("Error!", "Network response was not ok", "bg-danger");
-        throw new Error("Network response was not ok");
-      }
-      // Show success toast on success
-      showToast("Success!", "Thanks for reaching us!!", "bg-success");
-    })
-    .catch((error) => {
-      // Show error toast on failure
-      showToast(
-        "Error!",
-        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
-        "bg-danger"
-      );
-      console.error(
-        "Failure!!! We couldn't save your details, properly. Please try again in few minutes",
-        error
-      );
-    });
+      .then((response) => response.json())
+      .then((result) => {
+          // Check if the request was successful
+          if (result.success) {
+              showFormMessage(containerId, "Success", "Thanks for getting in touch. We will be in contact soon.", "text-success");
+          } else {
+              showFormMessage(containerId, "Error", "Failure!!! Please try again later.", "text-danger");
+          }
+      })
+      .catch((error) => {
+          showFormMessage(containerId, "Error", "Failure!!! Please try again later.", "text-danger");
+          console.error("Failure!!! Please try again later.", error);
+      });
 }
 
-function showToast(title, message, bgColorClass) {
-  // Use Bootstrap toast to show success or error message
-  var toastContainer = document.getElementById("toastContainer");
-  var toast = new bootstrap.Toast(toastContainer);
-
-  // Update toast content
-  toastContainer.querySelector(".toast-title").innerText = title;
-  toastContainer.querySelector(".toast-body").innerText = message;
-
-  // Update toast background color class
-  toastContainer.classList.remove("bg-success", "bg-danger");
-  toastContainer.classList.add(bgColorClass);
+// Function to display messages below the form
+function showFormMessage(containerId, title, message, textColorClass) {
+  var formContainer = document.getElementById(containerId);
+  var formMessageContainer = formContainer.querySelector(".form-message");
 
 
-    var forms = document.forms;
-    for (var i = 0; i < forms.length; i++) {
-        forms[i].reset();
-    }
 
-  // Show the toast
-  toast.show();
+  // Create a div for the form message if it doesn't exist
+  if (!formMessageContainer) {
+      formMessageContainer = document.createElement("div");
+      formMessageContainer.classList.add("form-message");
+      formContainer.appendChild(formMessageContainer);
+  }
+
+ // Remove existing messages
+ formMessageContainer.innerHTML = "";
+
+  // Update form message content
+  var messageElement = document.createElement("p");
+  messageElement.className = textColorClass;
+  messageElement.innerText = `${title}: ${message}`;
+  formMessageContainer.appendChild(messageElement);
 }
+
